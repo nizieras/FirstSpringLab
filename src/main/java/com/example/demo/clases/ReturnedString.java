02560e5a12aefe524d6aabd70985f0acde780a33
Nikita <nikitakokosss@gmail.com>
2021-04-22 22:44:12 +0300

    New second commit of first spring lab with correcting MainController.java, ReturnedString.java and deleting StringAppHomePage.java

--- src/main/java/com/example/demo/clases/ReturnedString.java
+++ src/main/java/com/example/demo/clases/ReturnedString.java
@@ -4,12 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 @AllArgsConstructor
 public class ReturnedString {
     @JsonProperty("Результат")
--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -2,7 +2,6 @@ package com.example.demo.controllers;
 
 import com.example.demo.service.MyStringAction;
 import com.example.demo.clases.ReturnedString;
-import com.example.demo.service.StringAppHomePage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,23 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/stringAct")
 public class MainController {
 
     private MyStringAction myStringAction;
-    private StringAppHomePage stringActHome;
     private ReturnedString returnedString;
 
     @Autowired
-    public MainController(MyStringAction myStringAction, StringAppHomePage stringActHome)
+    public MainController(MyStringAction myStringAction)
     {
-        this.stringActHome = stringActHome;
         this.myStringAction = myStringAction;
-    }
-
-    @GetMapping("/")
-    public StringAppHomePage stringAct() {
-        return this.stringActHome;
     }
 
     @GetMapping("/toUpCase")
