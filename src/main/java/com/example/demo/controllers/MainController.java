8491ca6da5d328236101698bd32b1975b9a2d66f
Nikita <nikitakokosss@gmail.com>
2021-04-26 22:35:19 +0300

    Adding interface.........

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -2,36 +2,29 @@ package com.example.demo.controllers;
 
 import com.example.demo.service.MyStringAction;
 import com.example.demo.clases.ReturnedString;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class MainController {
 
-    private MyStringAction myStringAction;
-    private ReturnedString returnedString;
-
-    @Autowired
-    public MainController(MyStringAction myStringAction)
-    {
-        this.myStringAction = myStringAction;
-    }
+    private final MyStringAction myStringAction;
 
     @GetMapping("/toUpCase")
     public ReturnedString transmittedStrToUpCase(@RequestParam String string){
-        return new ReturnedString(myStringAction.stringToUpCase(string));
+        return myStringAction.stringToUpCase(string);
     }
 
     @GetMapping("/toLowCase")
     public ReturnedString transmittedStrToLowCase(@RequestParam String string){
-        return new ReturnedString(myStringAction.stringToLowCase(string));
+        return myStringAction.stringToLowCase(string);
     }
 
     @GetMapping("/union")
     public ReturnedString transmittedStrUnion (@RequestParam String string, @RequestParam String secString){
-        return new ReturnedString (myStringAction.stringUnion(string, secString));
+        return myStringAction.stringUnion(string, secString);
     }
 }
